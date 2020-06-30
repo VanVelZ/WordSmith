390785269e53c3ef44abf3a5820b599e5d02c962
Zachary <zachofasunset@me.com>
2020-06-30 18:36:51 -0500

    Initial Commit

---
+++ WordSmith/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordSmith
+//
+//  Created by Zach Vandervelden on 6/30/20.
+//  Copyright © 2020 Zach Vandervelden. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
